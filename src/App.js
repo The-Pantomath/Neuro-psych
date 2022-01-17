3f1d36cbe98ce748469c2d2dd09b5ba8e6d1d281
pandyamarut <pandyamarut@gmail.com>
2022-01-17 22:22:26 +0530

    Change readme
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Trafficgraph } from './components/traffic-graph';
 
 function App() {
   return (
+    <div style={{backgroundColor: 'black'}}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{color: 'white'}}>
+        Hello! Welcome to the Neuro psych! Assume this canvas as your brain. 
+      </div>
+     <Trafficgraph />
+    </div>
     </div>
   );
 }
